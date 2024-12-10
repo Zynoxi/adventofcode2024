40d43b1e2ae844d3b3c63872fd31dd762a95098b
zynoxi <felix@ammenhaeuser.org>
2024-12-10 19:50:49 +0100

    chore: Initial commit

---
+++ src/main/java/de/fl3xi/Main.java
@@ -0,0 +1,9 @@
+package de.fl3xi;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
